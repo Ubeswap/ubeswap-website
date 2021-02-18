f14528d06d2f937cc85b6f4bd4c7853faf148ab9
Ian Macalinao <github@igm.pub>
2021-02-18 00:05:47 -0600

    Match gradient

--- src/lib/styles/globalStyles.ts
+++ src/lib/styles/globalStyles.ts
@@ -5,9 +5,21 @@ export const globalStyles = css`
     box-sizing: border-box;
   }
 
+  html {
+    background-color: rgb(44, 47, 54);
+  }
+
   body {
     font-family: Archivo, sans-serif;
-    background: #2e3338;
     color: #fff;
+
+    min-height: 100vh;
+    background-position: 0px -30vh;
+    background-repeat: no-repeat;
+    background-image: radial-gradient(
+      50% 50% at 50% 50%,
+      rgba(136, 120, 195, 0.1) 0%,
+      rgba(33, 36, 41, 0) 100%
+    );
   }
 `;
